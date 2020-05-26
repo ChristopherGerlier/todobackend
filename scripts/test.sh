e5a5e5c69a1d83119e938d7b7c249fea06de6608
Christopher <gerlier@cgerlierG3.ad.crfhealth.com>
2020-05-26 15:13:58 +0300

    Added release

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,9 @@
 # Activate virtual environment
 . /appenv/bin/activate
 
+# Download requirements to build cache
+pip download -d /build -r requirements_test.txt --no-input
+
 # Install application test requirements
 pip install -r requirements_test.txt
 
